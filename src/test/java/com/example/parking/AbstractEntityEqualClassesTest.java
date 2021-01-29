277c542c0aa8c974da37ed12519c38ac75741571
hamza bouabda <hbouabda@TSL.corp.thales>
2021-01-29 14:05:54 +0100

    abstract test

---
+++ src/test/java/com/example/parking/AbstractEntityEqualClassesTest.java
@@ -0,0 +1,54 @@
+package com.example.parking;
+
+import org.junit.jupiter.api.Test;
+
+public class AbstractEntityEqualClassesTest {
+
+    public static final int ID_1 = 1;
+
+    /*@Test
+    public void testHeritanceClass() {
+        Observable o1 = new Observable();
+        Observable2 o2 = new Observable2();
+        o1.setId(ID_1);
+        o2.setId(ID_1);
+
+        boolean equals1 = o1.equals(o2);
+        boolean equals2 = o2.equals(o1);
+
+        assertThat(equals1).isTrue();
+        assertThat(equals2).isTrue();
+    }
+
+    @Test
+    public void testDifferentClass() {
+        Observable o1 = new Observable();
+        Plant o2 = new Plant();
+        o1.setId(ID_1);
+        o2.setId(ID_1);
+
+        boolean equals1 = o1.equals(o2);
+        boolean equals2 = o2.equals(o1);
+
+        assertThat(equals1).isFalse();
+        assertThat(equals2).isFalse();
+    }
+
+    @Test
+    public void testSameClass() {
+        Observable o1 = new Observable();
+        Observable o2 = new Observable();
+        o1.setId(ID_1);
+        o2.setId(ID_1);
+
+        boolean equals1 = o1.equals(o2);
+        boolean equals2 = o2.equals(o1);
+
+        assertThat(equals1).isTrue();
+        assertThat(equals2).isTrue();
+    }
+
+    private static class Observable2 extends Observable {
+    }*/
+}
+
